134e58f55475243ac19f2bc1e525e37c96113222
Dan Sweeney <sweeneyd@vt.edu>
2019-06-07 12:08:49 -0400

    Add PNP Code

--- ParticleTracing/particle_flow.py
+++ ParticleTracing/particle_flow.py
@@ -8,7 +8,7 @@ ifilename = sys.argv[1]
 velocity_filename = sys.argv[2]
 tracers = int(sys.argv[3])
 S = float(sys.argv[4])
-ds = float(sys.argv[5])
+delta_s = float(sys.argv[5])
 
 ofilename = ifilename.split('.')[0]+'.xml'
 mesh = Mesh(ofilename)
@@ -42,8 +42,8 @@ def func(x, x_0, dt):
 def funcjac(x, x_0, dt):
     return np.eye(3)-jac(x)*dt
 
-N = int(S/ds)
-eps = 1.0
+N = int(S/delta_s)
+eps = 0.01
 x_min = -1.0
 x_max = 1.0
 y_min = -1.0
@@ -62,21 +62,21 @@ for tr in range(tracers):
                     eps,])
     x_n = x_0
     t_n = 0.0
-    print(x_0)
     for i in range(N):
         u_n = vel(x_n)
         print(tr, t_n, x_n[0], x_n[1], x_n[2], u_n[0], u_n[1], u_n[2], file=fout, sep=',')
         if x_n[0] >= x_max:
             break
-        dt = ds/np.linalg.norm(u_n)
+        dt = delta_s/np.linalg.norm(u_n)
         t_n += dt
         # Crank--Nicolson scheme
         x_n1 = x_n + u_n*dt
-        print(x_n1)
         x_n2 = fsolve(func, x_n1, args=(x_n, dt), fprime=funcjac)
         x_n = x_n2
-        if x_n[2] <= z_min+eps or x_n[2] >= z_max-eps \
-           or np.sqrt(x_n[0]**2 + x_n[1]**2) >= np.sqrt(x_max**2 + y_max**2)-eps:
+
+        if mesh.bounding_box_tree().compute_first_entity_collision(Point(x_n)) < mesh.num_cells():
+        # if x_n[2] <= z_min+eps or x_n[2] >= z_max-eps \
+        #    or np.sqrt(x_n[0]**2 + x_n[1]**2) >= np.sqrt(x_max**2 + y_max**2)-eps:
             break
     print("\n\n", file=fout)
 fout.close()
